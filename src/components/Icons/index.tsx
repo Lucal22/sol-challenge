b30f32821377c81b2c43b64e919fa11f20bb1af0
lucaldev <lucaldev22@gmail.com>
2023-10-28 13:45:37 -0300

    feat: importing svg icons.

---
+++ src/components/Icons/index.tsx
@@ -0,0 +1,103 @@
+import avatar from "./avatar.svg";
+
+export default {
+  avatar,
+};
+
+export function Home() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <g clip-path="url(#clip0_5332_478)">
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M14.5714 21.5C14.5714 21.7761 14.7953 22 15.0714 22H20.5C20.7761 22 21 21.7761 21 21.5V11.2451C21 10.5371 20.5824 9.56469 20.0673 9.08176L12.8993 2.36161C12.3782 1.87308 11.5445 1.88284 11.0239 2.37091L3.94263 9.00973C3.42045 9.49928 3 10.4633 3 11.1662C3 11.1662 3 11.5708 3 11.7732C3 14.2853 3 19.8134 3 21.501C3 21.7771 3.22386 22 3.5 22H8.92857C9.20471 22 9.42857 21.7761 9.42857 21.5V15C9.42857 14.7239 9.65243 14.5 9.92857 14.5H14.0714C14.3476 14.5 14.5714 14.7239 14.5714 15V21.5Z"
+          fill="#A076F2"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_5332_478">
+          <rect width="24" height="24" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
+
+export function Add() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <g clip-path="url(#clip0_5332_471)">
+        <path
+          d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM17 13H13V17H11V13H7V11H11V7H13V11H17V13Z"
+          fill="#9E9E9E"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_5332_471">
+          <rect width="24" height="24" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
+
+export function Search() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <g clip-path="url(#clip0_5332_464)">
+        <path
+          d="M17.1527 15.0943H16.0686L15.6844 14.7238C17.0292 13.1595 17.8388 11.1286 17.8388 8.91938C17.8388 3.99314 13.8456 0 8.91938 0C3.99314 0 0 3.99314 0 8.91938C0 13.8456 3.99314 17.8388 8.91938 17.8388C11.1286 17.8388 13.1595 17.0292 14.7238 15.6844L15.0943 16.0686V17.1527L21.9554 24L24 21.9554L17.1527 15.0943ZM8.91938 15.0943C5.50257 15.0943 2.74443 12.3362 2.74443 8.91938C2.74443 5.50257 5.50257 2.74443 8.91938 2.74443C12.3362 2.74443 15.0943 5.50257 15.0943 8.91938C15.0943 12.3362 12.3362 15.0943 8.91938 15.0943Z"
+          fill="#9E9E9E"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_5332_464">
+          <rect width="24" height="24" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
+
+export function Favorites() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <g clip-path="url(#clip0_5332_457)">
+        <path
+          d="M12 21.35L10.55 20.03C5.4 15.36 2 12.28 2 8.5C2 5.42 4.42 3 7.5 3C9.24 3 10.91 3.81 12 5.09C13.09 3.81 14.76 3 16.5 3C19.58 3 22 5.42 22 8.5C22 12.28 18.6 15.36 13.45 20.04L12 21.35Z"
+          fill="#9E9E9E"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_5332_457">
+          <rect width="24" height="24" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
