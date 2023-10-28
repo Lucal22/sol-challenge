ead279aa133c5a5d85f2a876a7a9c3c3506d63d0
lucaldev <lucaldev22@gmail.com>
2023-10-28 11:23:15 -0300

    feat: Adding fonts and styles.

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,5 +2,18 @@ export const theme = {
   colors: {
     white: "#FFFFFF",
     black: "#000000",
+    purple: "#A076F2",
+    title: "#555555",
+    details: "#757575",
+    border: "#E0E0E0",
+    nav: "#9E9E9E",
+  },
+  background: {
+    gray: "#F7F7F7",
+    white: "#FFFFFF",
+  },
+  font: {
+    bebas: `'Bebas Neue', sans-serif`,
+    roboto: `'Roboto', sans-serif`,
   },
 };
