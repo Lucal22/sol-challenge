af8b462c26c4231d128fe5e31f8b4b028e636e0d
lucaldev <lucaldev22@gmail.com>
2023-10-29 12:12:47 -0300

    feat: Adding homepage component and react query config to the App component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import HomePage from "./container/HomePage";
 import Header from "./container/Header";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 function App() {
+  const client = new QueryClient();
+
   return (
-    <>
+    <QueryClientProvider client={client}>
       <Header />
-    </>
+      <HomePage />
+    </QueryClientProvider>
   );
 }
 
