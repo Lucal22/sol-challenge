e5dafd92f7be71ea4bf20c3636343e5896d71f59
lucaldev <lucaldev22@gmail.com>
2023-10-30 01:14:13 -0300

    feat:II

--- src/container/BookPage/index.tsx
+++ src/container/BookPage/index.tsx
@@ -1,8 +1,21 @@
 import { useParams } from "react-router-dom";
 import * as Styled from "./styles";
+import fetchBook from "../../hooks/useBook";
+import { useQuery } from "react-query";
 
 export default function BookPage() {
   const { id } = useParams();
 
-  return <Styled.Container>{id}</Styled.Container>;
+  const { data, isLoading } = useQuery({
+    queryFn: () => fetchBook(id as string),
+    queryKey: "Book",
+    staleTime: Infinity,
+  });
+  const book = data?.data.book;
+  return (
+    <Styled.Container>
+      {isLoading ? <div>loading</div> : null}
+      {book ? <>{book?.name}</> : null}
+    </Styled.Container>
+  );
 }
